8821ee2a6e94d337e90242bf86b9f69a045a78d8
willyros01 <willyros01@gmail.com>
2026-09-04 10:31:26 -0400

    Bump New Message cleanup test build to 0.9.4.7

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 /* FIDUNIO single authoritative release version. Update this file for each new release. */
 globalThis.FIDUNIO_RELEASE = Object.freeze({
-  version: "0.9.4.6"
+  version: "0.9.4.7"
 });
